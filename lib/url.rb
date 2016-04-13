6c186a73355fcfee1abd2d24743e614abe0ca5a8
Rodrigo Martins <rodrigo@rrmartins.com>
2016-04-13 13:22:45 -0300

    lib to get and parse of url

--- lib/url.rb
+++ lib/url.rb
@@ -1,2 +1,15 @@
-class Url
+require 'uri'
+require 'net/http'
+require 'json'
+
+module RubyApplicants
+  class Url
+    def initialize(url)
+      @uri = URI(url)
+    end
+
+    def parse(params={})
+      JSON.parse Net::HTTP.post_form(@uri, params).body
+    end
+  end
 end
